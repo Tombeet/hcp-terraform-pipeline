eb5edefd11093958df6a358e860938de7980ea0a
Tombeet <107788612+Tombeet@users.noreply.github.com>
2026-01-30 19:10:42 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-provider "aws" {
-  region = "us-east-1" # You can change this to your preferred region
-}
 
 resource "aws_iam_user" "lb" {
   name = "hcp-test"
